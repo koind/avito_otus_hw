2054adf0940308c76e6e44c6b3807f60a51ee74f
koind <krosava_15d@mail.ru>
2022-05-16 22:28:14 +0600

    Added new business-logic

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,52 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+
+	if _, err := strconv.Atoi(str); err == nil {
+		return "", ErrInvalidString
+	}
+
+	var (
+		b       strings.Builder
+		prevStr rune
+	)
+
+	sl := []rune(str)
+
+	for i, r := range str {
+		if unicode.IsNumber(r) && i == 0 {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(prevStr) && unicode.IsDigit(r) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsLower(r) && unicode.IsLower(prevStr) {
+			b.WriteString(string(prevStr))
+		}
+
+		if unicode.IsLower(prevStr) && unicode.IsDigit(r) {
+			b.WriteString(strings.Repeat(string(prevStr), int(r-'0')))
+		}
+
+		if unicode.IsLower(r) && i == len(sl)-1 {
+			b.WriteString(string(r))
+		}
+
+		prevStr = r
+	}
+
+	return b.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		// new tests case
+		{input: "d3km4", expected: "dddkmmmm"},
+		{input: "a5", expected: "aaaaa"},
+		{input: "a", expected: "a"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
