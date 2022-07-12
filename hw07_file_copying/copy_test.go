598808523acb7714c1cdabe0eb0cbe8dc53bc049
koind <krosava_15d@mail.ru>
2022-07-12 20:11:29 +0600

    Added solution for hw7

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,35 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Test /dev/urandom", func(t *testing.T) {
+		err := Copy("/dev/urandom", "/tmp/", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("Test exceeded offset", func(t *testing.T) {
+		tempFile, err := ioutil.TempFile(".", "out_")
+		if err != nil {
+			t.FailNow()
+		}
+		defer func() {
+			tempFile.Close()
+			os.Remove(tempFile.Name())
+		}()
+
+		err = Copy("./testdata/input.txt", tempFile.Name(), 10000, 0)
+		require.Equal(t, ErrOffsetExceedsFileSize, err)
+	})
+
+	t.Run("Non-existent file opening", func(t *testing.T) {
+		err := Copy("./lsdkjflajflksdjfls;fk", "doesn't_matter.jpg", 0, 0)
+		require.Error(t, err)
+	})
 }
