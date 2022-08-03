99e45a0779619a569286c18ed860342df95781bd
koind <krosava_15d@mail.ru>
2022-08-04 00:07:14 +0600

    Added solution for hw9

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,24 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrValidationLength   = errors.New("invalid: length")
+	ErrValidationMinimum  = errors.New("invalid: minimum")
+	ErrValidationMaximum  = errors.New("invalid: maximum")
+	ErrValidationContains = errors.New("invalid: not contains")
+	ErrNotStruct          = errors.New("not struct is passed to validation")
+	ErrNoRule             = errors.New("no such validation rule")
+	ErrValidationRegexp   = errors.New("invalid: regexp")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +27,148 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	buff := strings.Builder{}
+
+	for _, e := range v {
+		buff.WriteString(fmt.Sprintf("Field: %s, Error: %v\n", e.Field, e.Err))
+	}
+
+	return buff.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	vErrors := ValidationErrors{}
+	e := reflect.ValueOf(v)
+
+	if e.Kind() != reflect.Struct {
+		return ErrNotStruct
+	}
+
+	for i := 0; i < e.NumField(); i++ {
+		field := e.Type().Field(i)
+		varName := field.Name
+		varTag := field.Tag
+		if varTag == "" {
+			continue
+		}
+
+		varValue := e.Field(i).Interface()
+
+		if e.Field(i).Kind() == reflect.Slice {
+			for _, sliceVal := range varValue.([]string) {
+				err := validateValue(varTag, sliceVal)
+				if err != nil {
+					if !isValidationError(&err) {
+						return err
+					}
+					vErrors = append(vErrors, ValidationError{Field: varName, Err: err})
+				}
+			}
+		} else {
+			err := validateValue(varTag, varValue)
+			if err != nil {
+				if !isValidationError(&err) {
+					return err
+				}
+				vErrors = append(vErrors, ValidationError{Field: varName, Err: err})
+			}
+		}
+
+	}
+
+	if len(vErrors) > 0 {
+		return vErrors
+	}
+
 	return nil
+}
+
+func isValidationError(err *error) bool {
+	if errors.Is(*err, ErrValidationLength) ||
+		errors.Is(*err, ErrValidationMinimum) ||
+		errors.Is(*err, ErrValidationMaximum) ||
+		errors.Is(*err, ErrValidationContains) ||
+		errors.Is(*err, ErrNotStruct) ||
+		errors.Is(*err, ErrNoRule) ||
+		errors.Is(*err, ErrValidationRegexp) {
+		return true
+	}
+
+	return false
+}
+
+func validateValue(varTag reflect.StructTag, varValue interface{}) error {
+	if varTagVal := varTag.Get("validate"); varTagVal != "" {
+		valRules := strings.Split(varTagVal, "|")
+
+		for _, rawRule := range valRules {
+			valRule := strings.Split(rawRule, ":")
+
+			if len(valRule) != 2 {
+				return ErrNoRule
+			}
+
+			rule := valRule[0]
+			val := valRule[1]
+
+			switch rule {
+			case "len":
+				intVal, err := strconv.Atoi(val)
+				if err != nil {
+					return err
+				}
+
+				if intVal != len(varValue.(string)) {
+					return ErrValidationLength
+				}
+			case "min":
+				intVal, err := strconv.Atoi(val)
+				if err != nil {
+					return err
+				}
+
+				if intVal > varValue.(int) {
+					return ErrValidationMinimum
+				}
+			case "max":
+				intVal, err := strconv.Atoi(val)
+				if err != nil {
+					return err
+				}
+
+				if intVal < varValue.(int) {
+					return ErrValidationMaximum
+				}
+			case "in":
+				inStr := strings.Split(val, ",")
+
+				if !contains(inStr, fmt.Sprintf("%v", varValue)) {
+					return ErrValidationContains
+				}
+			case "regexp":
+				matched, err := regexp.MatchString(val, fmt.Sprintf("%v", varValue))
+				if err != nil {
+					return err
+				}
+
+				if !matched {
+					return ErrValidationRegexp
+				}
+			default:
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+
+	return false
 }
